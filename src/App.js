003cb1dc101fc55ba18debb7dec77f723c7f8f46
Pankaj Monga <pankajmonga@Pankajs-MacBook-Air.local>
2020-04-01 14:53:54 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoInput from "./Components/TodoInput";
+import TodoList from "./Components/TodoList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>hello from app</h1>
+        <TodoInput></TodoInput>
+        <TodoList></TodoList>
+      </div>
+    );
+  }
 }
-
-export default App;
